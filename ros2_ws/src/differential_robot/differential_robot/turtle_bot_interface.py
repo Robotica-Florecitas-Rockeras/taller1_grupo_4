40c11ad371f2b14bb5a482d44b65c0d301836a40
Camilo Herrera <b.joya@uniandes.edu.co>
2025-02-16 22:37:20 -0500

    Req 3.1 without 3.2 :)

--- ros2_ws/src/differential_robot/differential_robot/turtle_bot_interface.py
+++ ros2_ws/src/differential_robot/differential_robot/turtle_bot_interface.py
@@ -4,9 +4,12 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton, QFileDialog, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget, QLineEdit, QPushButton, QFileDialog, QHBoxLayout, QDialog
 from PyQt5.QtCore import QThread, pyqtSignal, Qt
 from PyQt5.QtGui import QPixmap, QTransform, QPainter, QPen
+from typing import Callable
+
+
 
 
 class TurtleBotInterfaceNode(Node):
@@ -34,10 +37,9 @@ class TurtleBotInterfaceNode(Node):
     def update_turtle_orientation(self, msg:Float32):
 
         
-        self.get_logger().info(f"Angulo: " + str((msg.data)))
+        #self.get_logger().info(f"Angulo: " + str((msg.data)))
 
         theta = msg.data # Ángulo en radianes
-
         self.update_orientation_signal.emit(theta)
 
     
@@ -61,6 +63,7 @@ class MainUX(QMainWindow):
 
         self.setFixedSize(500, 500)
         self.setWindowTitle("Turtlebot Position")
+        self.move(480, 200)
 
         # Lienzo para el recorrido
         self.canvas = QPixmap(self.size())  
@@ -73,26 +76,15 @@ class MainUX(QMainWindow):
 
         # Tortuguita :)
         self.turtle_label = QLabel(self)
-        self.turtle_pixmap = QPixmap("/home/robotica/Desktop/taller1_grupo_4/ros2_ws/src/differential_robot/differential_robot/turtle.png")  
+        self.turtle_pixmap = QPixmap("/home/robotica/Desktop/Taller1/src/turtlebot_controller/turtlebot_controller/turtle.png")  
         if self.turtle_pixmap.isNull():
             print("Error: No se pudo cargar la imagen de la tortuga")
         self.turtle_label.setPixmap(self.turtle_pixmap)
         self.turtle_label.setScaledContents(True)
         self.turtle_label.setFixedSize(50, 50)  # Tamaño de la tortuga
 
-         # Input para el nombre del archivo
-        self.filename_input = QLineEdit(self)
-        self.filename_input.setPlaceholderText("Nombre de la gráfica")
-        self.filename_input.setFixedSize(100, 25)
-
-        # Botón para guardar la imagen
-        self.save_button = QPushButton("Guardar Gráfica", self)
-        self.save_button.setFixedSize(100, 25)
-        self.save_button.clicked.connect(self.save_graphic)
 
 
-
-        
         # Inicializar última posición
         self.last_x = None
         self.last_y = None
@@ -104,13 +96,17 @@ class MainUX(QMainWindow):
         self.setCentralWidget(central_widget)
 
 
+
+        # Boton de guardar ruta
+        self.save_line = QPushButton("Guardar", self)
+        self.save_line.setFixedSize(100, 35)
+        self.save_line.move(5,5)
+        self.save_line.setStyleSheet("background-color: #81C784; padding: 2px 8px; border: none; border-radius: 5px")
+        self.save_line.clicked.connect(self.save_window)
+
+
         # Instanciar Tortuguita 
         layout.addWidget(self.turtle_label)
-
-
-        # Instancia elemento del usuario
-        layout.addWidget(self.filename_input)  # Campo de texto para el nombre
-        layout.addWidget(self.save_button)  # Botón de guardar
 
 
         # Hilo del nodo de ROS
@@ -120,10 +116,24 @@ class MainUX(QMainWindow):
         self.ros_thread.start()
 
 
-    def save_graphic(self):
-        filename = self.filename_input.text().strip()
+        ventana = VentanaEmergente("¿Deseas una trayectoria del robot?", self.save_game)
+        ventana.exec_()
+
+
+    def save_game(self, filename):
+        pass
+
+    
+    def save_window(self):
+
+        save_wd = VentanaEmergente("¿Deseas guardar el recorrido del robot?", self.save_graphic)
+        save_wd.exec_()
+
+
+    def save_graphic(self, filename):
+
         if not filename:
-            filename = "trayecto"  # Nombre por defecto si no ingresan uno
+            filename = "trayecto.png"  # Nombre por defecto si no ingresan uno
 
         options = QFileDialog.Options()
         file_path, _ = QFileDialog.getSaveFileName(self, "Guardar Imagen", filename, "PNG Files (*.png);;JPEG Files (*.jpg)", options=options)
@@ -171,6 +181,70 @@ class MainUX(QMainWindow):
         transform.rotate(self.current_theta * 180 / 3.1416)
         rotated_pixmap = self.turtle_pixmap.transformed(transform, Qt.SmoothTransformation)
         self.turtle_label.setPixmap(rotated_pixmap)
+
+
+
+class VentanaEmergente(QDialog):
+    def __init__(self, message: str, ok_callback: Callable[[], None] = None,  close_callback: Callable[[], None] = None):
+        super().__init__()
+
+        self.setFixedSize(500, 500)
+        self.setWindowTitle("Guardar archivo")
+        self.move(480, 200)
+
+        self.ok_callback = ok_callback
+
+        layout = QVBoxLayout()
+
+        mensaje = QLabel(f"{str(message)}")
+        mensaje.setFixedWidth(500)
+        mensaje.setAlignment(Qt.AlignCenter)
+
+
+    
+        layout.addWidget(mensaje)
+        
+        # Instancia elemento del usuario
+
+        # Input para el nombre del archivo
+        self.filename_input = QLineEdit(self)
+        self.filename_input.setPlaceholderText("Nombre del archivo")
+        self.filename_input.setFixedHeight(35)
+        self.filename_input.setStyleSheet("border-radius: 10px; border: 2px solid gray; padding: 5px")
+        self.filename_input.setStyleSheet("padding: 30px; margin: auto")
+
+        layout.addWidget(self.filename_input)  # Campo de texto para el nombre
+
+
+
+        # Botón para guardar la imagen
+        self.save_button = QPushButton("Ok", self)
+        self.save_button.setFixedSize(150, 35)
+        self.save_button.setStyleSheet("background-color: #6fa3f5; padding: 2px 8px; border: none; border-radius: 5px")
+        self.save_button.clicked.connect(self.save)
+
+        self.close_button = QPushButton("Cerrar")
+        self.close_button.clicked.connect(self.close)
+        self.close_button.setFixedSize(150, 35)
+        self.close_button.setStyleSheet("background-color: #f27a7a; padding: 2px 8px; border: none; border-radius: 5px")
+
+
+        
+        user_layout = QHBoxLayout()
+        user_layout.setAlignment(Qt.AlignCenter)
+        
+        user_layout.addWidget(self.save_button)  # Botón de guardar
+        user_layout.addWidget(self.close_button) # Boton Cerrar
+
+        layout.addLayout(user_layout)
+
+        self.setLayout(layout)
+
+
+    def save(self):
+
+        self.ok_callback(self.filename_input.text().strip())
+
 
 
 def main():
